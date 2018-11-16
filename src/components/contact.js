04542e7c361890d6d038fc8ccb0fbe5ce381811b
paaaarv <pkumar@trinity.edu>
2018-11-16 17:32:08 -0600

    changed formatting of contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,18 +13,18 @@ export default class Contact extends React.Component{
       <div className="contact-div">
       <Grid>
       <Row>
-      <Col xs = '6' s = '6'md = "6" lg='6'>
+      <Col>
         <h4> CONTACT </h4>
       </Col>
-      <Col xs = '6' s = '6'md='6' lg= '6'>
+      <Col >
         <h4> CONNECT </h4>
       </Col>
       </Row>
       <Row>
-      <Col xs = '6' s = '6'md = "6" lg='6'>
+      <Col >
         <p className="contact-about"> allison.mandaville@gmail.com</p>
       </Col>
-      <Col xs = '6' s = '6'md='6' lg= '6'>
+      <Col >
         <a href="http://www.instagram.com/alpantsmandy" className='contact-about'> <i className="fab fa-instagram"></i> instagram </a>
       </Col>
       </Row>
